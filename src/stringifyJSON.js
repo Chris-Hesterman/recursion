a9aed94df0d9cd40ed0c5e2bad48cb4b2b2dc9a3
Chris Hesterman <spoiler14@comcast.net>
2020-01-19 12:27:06 -0800

    Implement code to pass test inputs 13 - 17, stringifyJSON.js
    Commented out first 12 input test models in fixtures.js
    as well as sub function code that passe them,
    in order to focus on code to pass last 8 input tests.
    will combine functionalities after all test inputs passed.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,49 +4,70 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-let result = '';
+let result = "";
 
-  function checkArray(obj) {
-    if (Array.isArray(obj)) {
-      let tempArr = obj.map(function(item, index, obj) {
-        if (Array.isArray(item) && item.length === 0) {
-          return `[${new Array()}]`;
-        }
-        if (Array.isArray(item)) {
-            return checkArray(item);
-        } else {
-          if (typeof item === 'string') {
-            return `"${item}"`;
-          } else {
-            return item;
-          }
-        }
-      });
-    return `[${tempArr}]`;
-    } else {
-      if (typeof obj === 'string') {
-        return '"' + obj +'"';
-      } else {
-        return `${obj}`;
-      }
-    }
-  }
-
-
-
-  // function checkObject(obj) {
-  //   if (Object.keys(obj).length === 0 && obj.constructor === Object) {
-  //     result += `{}`;
-  //       return;
+  // function checkArray(obj) {
+  //   if (Array.isArray(obj)) {
+  //     let tempArr = obj.map(function(item, index, obj) {
+  //       if (Array.isArray(item) && item.length === 0) {
+  //         return `[${new Array()}]`;
+  //       }
+  //       if (Array.isArray(item)) {
+  //           return checkArray(item);
+  //       } else {
+  //         if (typeof item === 'string') {
+  //           return `"${item}"`;
+  //         } else {
+  //           return item;
+  //         }
+  //       }
+  //     });
+  //   return `[${tempArr}]`;
+  //   } else {
+  //     if (typeof obj === 'string') {
+  //       return '"' + obj +'"';
+  //     } else {
+  //       return `${obj}`;
+  //     }
   //   }
   // }
 
 
 
+  function checkObject(obj) {
 
-  console.log(checkArray(obj));
+    if (typeof obj === 'string') {
+      return '"' + obj + '"';
+    } else if (obj === null || obj.constructor === Boolean) {
+      return obj;
+    }
+    if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+        return `{}`;
+    }
+    if (Object.keys(obj).length > 0 && obj.constructor === Object) {
+      let newObj = ''
+      let objKeys = Object.keys(obj);
+      for (let i = 0; i < objKeys.length; i++) {
+        let key = `${objKeys[i]}`;
+        let val = checkObject(obj[objKeys[i]]);
+        newObj += `"${key}":`;
+        newObj += val;
+        if (i + 1 < objKeys.length) {
+          newObj += ',';
+        }
+
+      }
+      newObj = `{${newObj}}`;
+      return newObj;
+    }
+  }
+
+
+
+
+  // console.log(checkObject(obj));
   // console.log(checkObject(obj));
   // isArray = checkArray(obj);
   // isObject = checkObject(obj);
-  return checkArray(obj);
+  return `${checkObject(obj)}`;
 };
